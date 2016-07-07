7199b62a92be770b2e08c3a7ca400ebe72ce24b2
codeamac17 <codeamac17@gmail.com>
2016-07-07 13:01:03 -0500

    Terminologia Oop

---
+++ Terminologia Oop.rb
@@ -0,0 +1,29 @@
+
+# Terminología Oop
+
+# Programación orientada a objetos (OOP)
+
+# Objetivos Académicos
+# Comprender la terminología de OOP.
+# Actividades
+# Para este ejercicio hay que juntarse en equipos.
+
+# De los términos siguientes explica en 2 o 3 oraciones cada concepto. Y 
+# ejemplifícalo con: dibujos, pseudo-código o código.
+
+# Inheritance
+# Composition
+# Encapsulation
+# Duck Typing
+# The Law of Demeter (ej.)
+# Overriding Methods (and using super)
+# Modules
+# Scope
+# Private vs Public Methods
+# Instance vs Class methods and variables
+# Polymorphism
+# Separation of Concerns
+# Recursos Externos
+# Practical Object Oriented Programming in Ruby (POODR)
+
+1º 
