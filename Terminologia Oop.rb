0b954916dc1769e7479f7e40a511288973c77495
codeamac01 <codeamac01@gmail.com>
2016-07-11 14:02:50 -0500

    agregacion de Argument Order

--- Terminologia Oop.rb
+++ Terminologia Oop.rb
@@ -25,5 +25,43 @@
 # Separation of Concerns
 # Recursos Externos
 # Practical Object Oriented Programming in Ruby (POODR)
+####################################################################################################################################################################################################################################
+# #1º Inheritance
+# Herencia
+# Herencia es cuando una clase hereda un comportamiento desde otra clase. La clase que está heredando comportamiento se le denomina subclase y la clase que le hereda ese comportamiento se llama superclase.
 
-1º 
+# Una ventaja de la herencia es que las clases que en una jerarquía están en un nivel inferior, heredan las características de las clases de niveles superiores; y además, pueden añadir sus propias características.
+
+# Por ejemplo, todos los gatos y perros son mamíferos. Si todos los mamíferos respiran, la clase gato y la clase perro heredan de la clase mamífero esta característica de respirar...
+
+# # superclase 
+
+class Mammal
+  def breath
+    puts 'inspirar, espirar'
+  end
+end
+
+# el símbolo < indica que 'Cat' es una subclase de Mammal
+
+class Cat < Mammal
+  def meow
+    puts 'Miaaaaaaaaaaau'
+  end
+end
+
+# el símbolo < indica que 'Dog' es una subclase de Mammal
+
+class Dog < Mammal
+  def bark
+    puts 'Guauu Guauu'
+  end
+end
+
+catty = Cat.new
+doggy = Dog.new
+catty.breath
+catty.meow
+doggy.breath
+doggy.bark
+# En el ejemplo anterior podemos ver otra ventaja de la herencia, que es el poder extraer comportamientos comunes (breath) desde clases que comparten ese comportamiento (Cat y Dog) y moverlo a una superclase (Mammal). Esta es una manera de mantener la lógica en un sólo lugar.
